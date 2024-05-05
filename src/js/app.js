dd52c472c547fc61537cddb2649cf560df7e9d74
Guillermo C.G <151522926+GuilleMarejada@users.noreply.github.com>
2024-05-05 11:42:23 +0000

    asignamos place holders a las variables

--- src/js/app.js
+++ src/js/app.js
@@ -28,13 +28,39 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  //role
+  let role = variables.role;
+  if (variables.role == null) {
+    role = "Role";
+  }
+  //Name
+  let name = variables.name;
+  if (variables.name == null) {
+    name = "Name";
+  }
+  //Last Name
+  let lastName = variables.lastName;
+  if (variables.lastName == null) {
+    lastName = "Last Name";
+  }
+  //city
+  let city = variables.city;
+  if (variables.city == null) {
+    city = "City";
+  }
+  //country
+  let country = variables.country;
+  if (variables.country == null) {
+    country = "Country";
+  }
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastName}</h1>
-          <h2>${variables.role}</h2>
-          <h3>${variables.city}, ${variables.country}</h3>
+          <h1>${name} ${lastName}</h1>
+          <h2>${role}</h2>
+          <h3>${city}, ${country}</h3>
           <ul class="${variables.socialMediaPosition}">
             <li><a href="${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
             <li><a href="${variables.github}"><i class="fab fa-github"></i></a></li>
@@ -43,11 +69,6 @@ function render(variables = {}) {
           </ul>
         </div>
     `;
-  ///role
-
-  if (variables.role == null) {
-    variables.role = " ";
-  }
 }
 
 /**
